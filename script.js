23a3c5c5c33a6dab3a6cb12dc1982cfbcd97baad
Niksa1101 <130042882+Niksa1101@users.noreply.github.com>
2025-05-18 18:44:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function closeLightbox() {
   const lightbox = document.getElementById('lightbox');
   lightbox.classList.add('hidden');
 }
+
+function toggleLanguage() {
+  const elements = document.querySelectorAll('[data-en]');
+  const isEnglish = document.documentElement.lang === 'en';
+  elements.forEach(el => {
+    el.textContent = el.getAttribute(isEnglish ? 'data-sr' : 'data-en');
+  });
+  document.documentElement.lang = isEnglish ? 'sr' : 'en';
+}
